b935060235edf7e77582dd0be011de6932c25a71
Roger Massana <rogermassana2002@gmail.com>
2022-08-23 18:00:33 +0200

    Method that returns all posible knight positions

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,13 @@
+# frozen_string_literal: true
+
+require_relative 'graph'
 class Game
   def initialize
-    @board = Array.new(8) { Array.new(8,0) }
+    @board = Array.new(8) { Array.new(8, 0) }
     @knight = [0, 0]
   end
 end
+
+game = Game.new
+graph = Graph.new
+graph.put_knight(3, 3)
--- lib/graph.rb
+++ lib/graph.rb
@@ -1,5 +1,19 @@
+# frozen_string_literal: true
+
 class Graph
   def initialize
     @root = nil
+    @knight_moves = []
+  end
+
+  def put_knight(x, y)
+    @knight_moves.push([x + 2, y + 1]) if x + 2 < 8 && y + 1 < 8
+    @knight_moves.push([x + 2, y - 1]) if x + 2 < 8 && (y - 1).positive?
+    @knight_moves.push([x + 1, y + 2]) if x + 1 < 8 && y + 2 < 8
+    @knight_moves.push([x + 1, y - 2]) if x + 1 < 8 && (y - 2).positive?
+    @knight_moves.push([x - 1, y + 2]) if (x - 1).positive? && y + 2 < 8
+    @knight_moves.push([x - 1, y - 2]) if (x - 1).positive? && (y - 2).positive?
+    @knight_moves.push([x - 2, y + 1]) if (x - 2).positive? && y + 1 < 8
+    @knight_moves.push([x - 2, y - 1]) if (x - 2).positive? && (y - 1).positive?
   end
 end
